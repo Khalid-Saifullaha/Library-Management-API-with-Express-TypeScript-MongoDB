fd0ece29ecb44c9f839e836f090a148811e0f360
Your Name <khalidsaifullaha3@gmail.com>
2025-08-23 11:30:56 +0600

    Update CORS origin url

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://library-management-ochre-ten.vercel.app",
+      "https://library-management-ochre-ten.vercel.app/api",
     ],
   })
 );
